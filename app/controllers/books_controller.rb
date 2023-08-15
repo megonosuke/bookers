d4d901948c5a65ef82aaf4bbe1c0ef10d770708d
MegumiKamata <141466592+megonosuke@users.noreply.github.com>
2023-08-15 00:33:28 +0000

    [Rest]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,50 @@
 class BooksController < ApplicationController
 
   def create
-    # book = Book.new(book_params)
-    # book.save
-    # redirect_to '/top'
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
-  
+
   def index
-    # @book = Book.index
+    @book = Book.new
+    @books= Book.all
+    # redirect_to '/show'
+
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
   def destroy
+    # flash[:notice] = "Book was successfully destroyed."
     book = Book.find(params[:id])
     book.destroy
-    # 
+    redirect_to '/books'
   end
-  
+
+  # def destroy
+  #   book = Book.find(params[:id])
+  #   book.destroy
+  #   redirect_to '/books'
+  #   # redirect_to '/index'
+  # end
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
